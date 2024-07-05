a386d52d077988c2c09edbb3d4e7928e15ddbce5
Osama <usama_gis@hotmail.com>
2024-07-05 03:19:11 +0300

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+//import Inter font and define latin as fallback
+import {Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400'});
